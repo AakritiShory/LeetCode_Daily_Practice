8fe23acf8f052bc292fecc515d7acaf8aff0b6ac
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-01-28 22:42:53 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (59.25%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        // for(int i=0;i<nums.length;i++){
+        //     int count=0;
+        //     int store=nums[i];
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[j]==store){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1){
+        //         return store;
+        //     }
+        // }
+        // return -1;
+
+        //---->OPTIMIZED---->
+        int xor=0;
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            int store=nums[i];
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==store){
-                    count++;
-                }
-            }
-            if(count==1){
-                return store;
-            }
+            xor=xor^nums[i];
         }
-        return -1;
-        
+        return xor;
     }
 }
