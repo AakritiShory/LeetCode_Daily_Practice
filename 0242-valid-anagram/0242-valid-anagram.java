dfef0172072003d1f5ad1259cbd6115f64b6de61
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-02-18 09:50:20 +0530

    Time: 6 ms (43.72%), Space: 42.9 MB (89.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,34 @@
 class Solution {
-    static HashMap<Character,Integer> freq(String s){
-        HashMap<Character,Integer> mpp=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            Character ch=s.charAt(i);
-            if(!mpp.containsKey(ch)){
-                mpp.put(ch,1);
-            }else{
-                mpp.put(ch,mpp.get(ch)+1);
-            }
-        }
-        return mpp;
-    }
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        HashMap<Character,Integer> mp1=freq(s);
-        HashMap<Character,Integer> mp2=freq(t);
-        return mp1.equals(mp2);
+    // static HashMap<Character,Integer> freq(String s){
+    //     HashMap<Character,Integer> mpp=new HashMap<>();
+    //     for(int i=0;i<s.length();i++){
+    //         Character ch=s.charAt(i);
+    //         if(!mpp.containsKey(ch)){
+    //             mpp.put(ch,1);
+    //         }else{
+    //             mpp.put(ch,mpp.get(ch)+1);
+    //         }
+    //     }
+    //     return mpp;
+    // }
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length()!=t.length())return false;
+    //     HashMap<Character,Integer> mp1=freq(s);
+    //     HashMap<Character,Integer> mp2=freq(t);
+    //     return mp1.equals(mp2);
         
+    // }
+    public boolean isAnagram(String s,String t){
+        if(s.length()!=t.length())return false;
+        //freq array
+        int[] count=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;//increment for s
+            count[t.charAt(i)-'a']--;//decrement for t
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0)return false;
+        }
+        return true;
     }
 }
