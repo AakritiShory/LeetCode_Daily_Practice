b8b0e6b2727377bf2c5768178422900c8d634a9d
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-02-18 08:50:22 +0530

    Time: 14 ms (30.2%), Space: 44.2 MB (59.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,21 @@
 class Solution {
-
-    static HashMap<Character,Integer> freq(String str){
-        HashMap<Character,Integer> mp=new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            Character ch=str.charAt(i);
-            if(!mp.containsKey(ch)){
-                mp.put(ch,1);
+    static HashMap<Character,Integer> freq(String s){
+        HashMap<Character,Integer> mpp=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            Character ch=s.charAt(i);
+            if(!mpp.containsKey(ch)){
+                mpp.put(ch,1);
             }else{
-                mp.put(ch,mp.getOrDefault(ch,0)+1);
+                mpp.put(ch,mpp.get(ch)+1);
             }
         }
-        return mp;
+        return mpp;
     }
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
+        if(s.length()!=t.length())return false;
         HashMap<Character,Integer> mp1=freq(s);
         HashMap<Character,Integer> mp2=freq(t);
         return mp1.equals(mp2);
-       
         
     }
 }
