937b207df9c907c359479d81f4c92fca039559fa
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-04-09 14:16:52 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,25 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* newList=new ListNode(0);//dummy node
-        ListNode* curr=newList;
-
-        while(list1 != NULL && list2 != NULL){
-            if(list1->val < list2->val){
-                 curr->next=list1;
-                 list1=list1->next;
-            }else{//equal aur greater vala case
-                curr->next=list2;
-                list2=list2->next;
-            }
-            curr=curr->next;
-
+             //recursive method
+        if(list1==NULL|| list2==NULL){
+            return list1==NULL ? list2:list1;
         }
-        //attach remaining nodes
-        if(list1 != NULL) curr->next=list1;
-        if(list2 != NULL) curr->next=list2;
+        //case1
+        if(list1->val <=list2->val){
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }else{
+            list2->next=mergeTwoLists(list1,list2->next);
+            return list2;
+        }
 
-        return newList->next; // Return merged list without dummy node
+        // ListNode* newList=new ListNode(0);//dummy node
+        // ListNode* curr=newList;
+
+        // while(list1 != NULL && list2 != NULL){
+        //     if(list1->val < list2->val){
+        //          curr->next=list1;
+        //          list1=list1->next;
+        //     }else{//equal aur greater vala case
+        //         curr->next=list2;
+        //         list2=list2->next;
+        //     }
+        //     curr=curr->next;
+
+        // }
+        // //attach remaining nodes
+        // if(list1 != NULL) curr->next=list1;
+        // if(list2 != NULL) curr->next=list2;
+
+        // return newList->next; // Return merged list without dummy node
 
     }
 };
