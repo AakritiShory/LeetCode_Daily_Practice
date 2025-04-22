1df86814e332ab0671b9c7afcbc6f5551589d2c6
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-04-22 14:42:16 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.51%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,15 +4,13 @@ public:
         int i=0;
         int j=s.size()-1;
         while(i<j){
-            int temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
-            i++;
-            j--;
+            // int temp=s[i];
+            // s[i]=s[j];
+            // s[j]=temp;
+            // i++;
+            // j--;
+            swap(s[i++],s[j--]);
         }
-
-      
-        
         
     }
 };
