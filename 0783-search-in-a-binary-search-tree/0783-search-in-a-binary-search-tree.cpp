26aaa4f083d0b7e2c853cd21050846ce9b85bfe9
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-04-26 23:31:59 +0530

    Time: 0 ms (100%), Space: 35.4 MB (61.73%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -12,18 +12,33 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        //agr root he null hai toh false value return
-        if(root==NULL) return NULL;
+    //     //agr root he null hai toh false value return
+    //     if(root==NULL) return NULL;
 
-        //agr root he target ke equal
-        if(root->val==val) return root;
+    //     //agr root he target ke equal
+    //     if(root->val==val) return root;
 
-        //value choti root se toh left part search
-        if(root->val>val){
-            return searchBST(root->left,val);
-        }else{
-            //value hai root se badi toh right part search
-            return searchBST(root->right,val);
+    //     //value choti root se toh left part search
+    //     if(root->val>val){
+    //         return searchBST(root->left,val);
+    //     }else{
+    //         //value hai root se badi toh right part search
+    //         return searchBST(root->right,val);
+    //     }
+       
+       //iterative way
+       //ek nayi node bana lo
+       TreeNode* temp=root;
+       while(temp!=NULL){
+        if(temp->val==val){
+            return temp; //node return krni hai
         }
+        if(temp->val>val){
+            temp=temp->left;
+        }else{
+            temp=temp->right;
+        }
+       }
+       return NULL;
     }
 };
