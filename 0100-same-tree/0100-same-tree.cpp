7bfb8d796599db7f649a309edc58106756c2821c
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-04-30 01:48:31 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.23%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,13 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {    //on oh
+class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL && q==NULL) return true;
         if(p==NULL || q==NULL) return false;
 
-        if(p->val!=q->val) return false;
-        return isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
