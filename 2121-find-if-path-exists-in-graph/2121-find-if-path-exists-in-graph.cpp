e82b64273c971bb185a47eec846e1e72b506157c
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-05-01 02:27:06 +0530

    Time: 637 ms (18.11%), Space: 359.1 MB (19.52%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    bool check(unordered_map<int,vector<int>>& mp,int s,int d,vector<bool>& vis){
-        if(s==d) return true;
-        //agr iss source pr aa chuke toh dobara nhi jaoge
-        //loop mei fas jaoge 0-1,1-0
-        if(vis[s]==true) return false;
-        //agr nhi hai toh
-        vis[s]=true;
-        //ab iss soruce se kaha ja skte ho
-        for(auto node:mp[s]){
-            if(check(mp,node,d,vis)==true) return true;
-        }
-        return false;
-    }
+   
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         //adjacency list
         unordered_map<int,vector<int>>mp;
@@ -26,6 +14,23 @@ public:
             mp[v].push_back(u); //v se u bhi edge
         }
         vector<bool> vis(n,false);
-        return check(mp,source,destination,vis);
+
+        queue<int> q;
+        q.push(source);
+        vis[source]=true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(node==destination) return true; //imp
+            for(auto i:mp[node]){
+                if(!vis[i]){
+                    q.push(i);
+                    vis[i]=true;
+                }
+            }
+
+        }
+        return false;
+       
     }
 };
