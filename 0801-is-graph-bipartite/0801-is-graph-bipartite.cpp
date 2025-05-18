878912d23bafc9fe975b6614809085fbc322b78d
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-05-18 23:56:34 +0530

    Time: 0 ms (100%), Space: 17.7 MB (14.29%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
-        //queue<int>q;
-        vector<int>color(V,-1);
-        // q.push(0);//start
-        // color[0]=0; //say 0 and 1 color apne pass
-
-        //disconected graph so i need for loop
+        vector<int>color(V,-1); //mark -1 for no color
+        //since graph is diconnected we will check for -1 visited nodes
         for(int i=0;i<V;i++){
             if(color[i]==-1){
                 queue<int>q;
-                q.push(i);
+                q.push(i); //from ith node it will start
                 color[i]=0;
-           
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
 
-
-            for(auto nei:graph[node]){
-                //if adj node not yet color ,u give opp color of node
-                if(color[nei]==-1){
-                    color[nei]=!color[node];
-                    q.push(nei);
-                    //if node has same color ,return false
-                }else if(color[nei]==color[node]){
-                    return false;
+                while(!q.empty()){ //bfs will begin
+                    int node=q.front();
+                    q.pop();
+                    //neighbor color check
+                    for(auto nei:graph[node]){
+                        //agr color nhi
+                        if(color[nei]==-1){
+                            //oop color kro
+                            color[nei]=!color[node];//imppp
+                            //push krdo
+                            q.push(nei);
+                            //agr color same hai node ke toh false
+                        }else if(color[nei]==color[node]){
+                                return false;
+                        }
+                    }
                 }
             }
         }
-         }
-        }
         return true;
-
     }
 };
