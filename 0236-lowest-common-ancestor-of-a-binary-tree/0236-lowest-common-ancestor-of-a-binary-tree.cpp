e36a0a3a4261a834e0c111046f7365a3f1bbeb74
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-05-20 12:17:51 +0530

    Time: 10 ms (77.85%), Space: 17.4 MB (67.62%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,21 +11,12 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL) return NULL;
-//agr p ya q root ke he equal nikle toh voh return
-        if(root == p || root == q) return root;
+        if(root==p||root==q) return root;
+        TreeNode* l=lowestCommonAncestor(root->left,p,q);
+        TreeNode* r=lowestCommonAncestor(root->right,p,q);
+        if(l!=NULL && r!=NULL) return root;
+        if(l!=NULL) return l;
+        else return r;
 
-//root ke left side se koi value mile toh voh lo
-//root ke right side se value mile toh voh lo
-        TreeNode* leftSide=lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightSide=lowestCommonAncestor(root->right,p,q);
-        
-//agr left side aur right side dono null nhi toh root he return
-        if(leftSide !=NULL && rightSide !=NULL) return root;
-
-//agr left side null nhi toh left return,koi toh value hogi
-//agr right side mei koi value hai null nahi hai toh voh return
-        if(leftSide !=NULL) return leftSide;
-        else return rightSide;
-        
     }
 };
