5fc4656906a3deb1c94d0f914b51ebc8c7760c80
Aakriti Shory <122432160+AakritiShory@users.noreply.github.com>
2025-05-22 09:08:53 +0530

    Time: 269 ms (13.99%), Space: 24 MB (12.81%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -19,9 +19,10 @@ public:
     }
     int diameterOfBinaryTree(TreeNode* root) {
         if(root==NULL) return 0;
-        int d1=height(root->left)+height(root->right);
-        int d2=diameterOfBinaryTree(root->left);
-        int d3=diameterOfBinaryTree(root->right);
-        return max(d1,max(d2,d3));
+
+        int d1=diameterOfBinaryTree(root->left);
+        int d2=diameterOfBinaryTree(root->right);
+        int d3=height(root->left)+height(root->right);
+        return max(d3,max(d1,d2));
     }
 };
